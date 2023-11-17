345478e840b5e9d173ec9e5c53623500f33d6406
m22027323 <fabien.marcuccini@etu.univ-amu.fr>
2023-11-17 14:25:11 +0100

    add private EmptyFigure()

---
+++ app/src/main/java/amazons/figures/EmptyFigure.java
@@ -0,0 +1,31 @@
+package amazons.figures;
+
+import amazons.board.Board;
+import amazons.board.Position;
+import amazons.player.PlayerID;
+
+public class EmptyFigure implements Figure {
+    public static EmptyFigure EMPTY_FIGURE;
+    private EmptyFigure(){
+        EMPTY_FIGURE = new EmptyFigure();
+    }
+    @Override
+    public boolean canMoveTo(Position position, Board board) {
+        return false;
+    }
+
+    @Override
+    public void moveTo(Position position, Board board) {
+
+    }
+
+    @Override
+    public void setPosition(Position position) {
+
+    }
+
+    @Override
+    public PlayerID getPlayerID() {
+        return null;
+    }
+}
