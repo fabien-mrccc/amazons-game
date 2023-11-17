0a59a928838a69a27d437fc177874984588f3fff
m22027323 <fabien.marcuccini@etu.univ-amu.fr>
2023-11-17 16:04:54 +0100

    comeback here to check if constant is always null add void testGetPlayerID() add void testCanMoveTo()

--- app/src/main/java/amazons/figures/EmptyFigure.java
+++ app/src/main/java/amazons/figures/EmptyFigure.java
@@ -29,7 +29,6 @@ public class EmptyFigure implements Figure {
         else{
             throw new IllegalArgumentException();
         }
-
          */
     }
 
--- app/src/test/java/amazons/figures/EmptyFigureTest.java
+++ app/src/test/java/amazons/figures/EmptyFigureTest.java
@@ -1,7 +1,18 @@
 package amazons.figures;
 
+import amazons.board.Position;
+import amazons.player.PlayerID;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
 public class EmptyFigureTest {
+    @Test
+    void testGetPlayerID(){
+        assertThat(EmptyFigure.EMPTY_FIGURE.getPlayerID()).isEqualTo(PlayerID.NONE);
+    }
 
-
-
+    @Test
+    void testCanMoveTo(){
+        assertThat(EmptyFigure.EMPTY_FIGURE.canMoveTo(new Position (0,0), null)).isEqualTo(false);
+    }
 }
