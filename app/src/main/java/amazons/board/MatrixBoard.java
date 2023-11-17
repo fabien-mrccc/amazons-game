a87ddb422a9af77c7978f19f58cace5f69e33afb
Fanani <amina.fanani@etu.univ-amu.fr>
2023-11-17 14:52:30 +0100

    add MatrixBoard(int numberOfColumns, int numberOfRows)

--- app/src/main/java/amazons/board/MatrixBoard.java
+++ app/src/main/java/amazons/board/MatrixBoard.java
@@ -1,9 +1,17 @@
 package amazons.board;
 
+import amazons.figures.EmptyFigure;
 import amazons.figures.Figure;
 
 public class MatrixBoard implements Board {
-    //private Figure[][] = new EmptyFigure();
+    private Figure[][] figures ;
+    public MatrixBoard(int numberOfColumns, int numberOfRows){
+        for(int x=0; x< numberOfColumns; x++){
+            for(int y=0; y< numberOfRows; y++){
+                figures[x][y] = EmptyFigure.EMPTY_FIGURE;
+            }
+        }
+    }
     @Override
     public void setFigure(Position position, Figure figure) {
 
