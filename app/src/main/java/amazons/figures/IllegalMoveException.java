0d4d0393a6cd1ff39c757782064927dccb644b7f
Fanani <amina.fanani@etu.univ-amu.fr>
2023-11-21 15:37:17 +0100

    add public IllegalMoveException(String message)

---
+++ app/src/main/java/amazons/figures/IllegalMoveException.java
@@ -0,0 +1,7 @@
+package amazons.figures;
+
+public class IllegalMoveException extends Exception{
+    public IllegalMoveException(String message){
+        System.out.println(message);
+    }
+}
--- app/src/main/java/amazons/figures/PositionException.java
+++
@@ -1,7 +0,0 @@
-package amazons.figures;
-
-public class PositionException extends RuntimeException{
-    public PositionException(String message){
-        System.out.println(message);
-    }
-}
