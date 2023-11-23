0941c55b56e8c59566c825b888945cda4091adc5
marcuccini <fabien.marcuccini@etu.univ-amu.fr>
2023-11-23 20:33:45 +0100

    fix public MatrixIterator(int numberOfColumns, int numberOfRows, T[][] matrix): remove unnecessary attributes

--- app/src/main/java/amazons/board/MatrixIterator.java
+++ app/src/main/java/amazons/board/MatrixIterator.java
@@ -3,15 +3,11 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class MatrixIterator<T> implements Iterator<T> {
-    private final int numberOfColumns;
-    private final int numberOfRows;
     private final Position lastPosition;
     private final T[][] matrix;
     private Position currentPosition = new Position(0, 0);
 
     public MatrixIterator(int numberOfColumns, int numberOfRows, T[][] matrix) {
-        this.numberOfColumns = numberOfColumns;
-        this.numberOfRows = numberOfRows;
         this.lastPosition = new Position(numberOfColumns - 1, numberOfRows - 1);
         this.matrix = matrix;
     }
@@ -27,7 +23,7 @@ public class MatrixIterator<T> implements Iterator<T> {
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        if (getCurrentPosition().getX() == numberOfColumns - 1) {
+        if (getCurrentPosition().getX() == getLastPosition().getX()) {
             currentPosition = new Position(0, getCurrentPosition().getY() + 1);
         }
         else{
