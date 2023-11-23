0c66e6815458f52e98125f96d5d29ca984827661
Fanani <amina.fanani@etu.univ-amu.fr>
2023-11-23 11:11:16 +0100

    fix public Iterator<Figure> iterator()

--- app/src/main/java/amazons/board/MatrixBoard.java
+++ app/src/main/java/amazons/board/MatrixBoard.java
@@ -2,6 +2,8 @@ package amazons.board;
 
 import amazons.figures.Figure;
 
+import java.util.Iterator;
+
 public class MatrixBoard extends AbstractBoard {
 
     private Figure[][] figures ;
@@ -18,5 +20,10 @@ public class MatrixBoard extends AbstractBoard {
     @Override
     public Figure getFigure(Position position) {
         return figures[position.getX()][position.getY()];
+    }
+
+    @Override
+    public Iterator<Figure> iterator() {
+        return new MatrixIterator<Figure>(getNumberOfColumns(),getNumberOfRows(),figures);
     }
 }
