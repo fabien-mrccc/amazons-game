e037b416de7c610f09bba4e71a7fe5b15fda252e
marcuccini <fabien.marcuccini@etu.univ-amu.fr>
2023-11-23 20:25:03 +0100

    add public Position getCurrentPosition() add public Position getLastPosition()

--- app/src/main/java/amazons/board/MatrixIterator.java
+++ app/src/main/java/amazons/board/MatrixIterator.java
@@ -1,6 +1,4 @@
 package amazons.board;
-
-import javax.swing.text.html.HTMLDocument;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -20,7 +18,7 @@ public class MatrixIterator<T> implements Iterator<T> {
 
     @Override
     public boolean hasNext() {
-        return !currentPosition.equals(lastPosition);
+        return !getCurrentPosition().equals(getLastPosition());
     }
 
     //TODO Demander à notre chargée de TP si c'est normal que next() doive mettre à jour currentPosition alors que le commentaire dans l'interface demande un simple retour
@@ -29,16 +27,24 @@ public class MatrixIterator<T> implements Iterator<T> {
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        if (currentPosition.getX() == numberOfColumns - 1) {
-            currentPosition = new Position(0, currentPosition.getY() + 1);
+        if (getCurrentPosition().getX() == numberOfColumns - 1) {
+            currentPosition = new Position(0, getCurrentPosition().getY() + 1);
         }
         else{
-            currentPosition = new Position(currentPosition.getX() + 1, currentPosition.getY());
+            currentPosition = new Position(getCurrentPosition().getX() + 1, getCurrentPosition().getY());
         }
-        return getTInMatrix(currentPosition);
+        return getTInMatrix(getCurrentPosition());
     }
 
     public T getTInMatrix(Position position) {
         return matrix[position.getX()][position.getY()];
+    }
+
+    public Position getCurrentPosition(){
+        return currentPosition;
+    }
+
+    public Position getLastPosition(){
+        return lastPosition;
     }
 }
