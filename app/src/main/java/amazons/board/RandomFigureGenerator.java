a24bddbb99f11739d68b9d60db5f7ddb0f4d1d3c
marcuccini <fabien.marcuccini@etu.univ-amu.fr>
2023-11-26 17:33:03 +0100

    fix public RandomFigureGenerator(Random random, List<MovableFigure> movableFigures, Iterator<Position> positionIterator)

--- app/src/main/java/amazons/board/RandomFigureGenerator.java
+++ app/src/main/java/amazons/board/RandomFigureGenerator.java
@@ -6,10 +6,10 @@ import amazons.figures.MovableFigure;
 import java.util.List;
 import java.util.Iterator;
 public class RandomFigureGenerator implements FigureGenerator{
-    private List<MovableFigure> figures;
-    public RandomFigureGenerator(Random random, List<MovableFigure> figures, Iterator<Position> positionIterator){
-        this.figures = figures;
-        for(MovableFigure movableFigure : figures){
+    private List<MovableFigure> movableFigures;
+    public RandomFigureGenerator(Random random, List<MovableFigure> movableFigures, Iterator<Position> positionIterator){
+        this.movableFigures = movableFigures;
+        for(MovableFigure movableFigure : movableFigures){
 
         }
     }
