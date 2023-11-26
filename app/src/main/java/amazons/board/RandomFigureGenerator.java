aa5375e1cc45625cbca7ff3d8241279e30e40fee
marcuccini <fabien.marcuccini@etu.univ-amu.fr>
2023-11-26 17:40:12 +0100

    fix public RandomFigureGenerator(Random random, List<MovableFigure> movableFigures, Iterator<Position> positionIterator): change for loop with this assignments

--- app/src/main/java/amazons/board/RandomFigureGenerator.java
+++ app/src/main/java/amazons/board/RandomFigureGenerator.java
@@ -7,11 +7,13 @@ import java.util.List;
 import java.util.Iterator;
 public class RandomFigureGenerator implements FigureGenerator{
     private List<MovableFigure> movableFigures;
+    private Random random;
+    private Iterator<Position> positionIterator;
+
     public RandomFigureGenerator(Random random, List<MovableFigure> movableFigures, Iterator<Position> positionIterator){
         this.movableFigures = movableFigures;
-        for(MovableFigure movableFigure : movableFigures){
-
-        }
+        this.random = random;
+        this.positionIterator = positionIterator;
     }
 
     @Override
