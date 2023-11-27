1e704a1123f9aa6f4e29b7214dec2d9fccea5eb6
marcuccini <fabien.marcuccini@etu.univ-amu.fr>
2023-11-27 21:00:36 +0100

    fix public T next(): add tempPosition

--- app/src/main/java/amazons/board/MatrixIterator.java
+++ app/src/main/java/amazons/board/MatrixIterator.java
@@ -5,7 +5,7 @@ import java.util.NoSuchElementException;
 public class MatrixIterator<T> implements Iterator<T> {
     private final Position lastPosition;
     private final T[][] matrix;
-    private Position currentPosition = new Position(0, -1);
+    private Position currentPosition = new Position(0, 0);
 
     public MatrixIterator(int numberOfColumns, int numberOfRows, T[][] matrix) {
         this.lastPosition = new Position(numberOfColumns - 1, numberOfRows - 1);
@@ -23,13 +23,16 @@ public class MatrixIterator<T> implements Iterator<T> {
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
+
+        Position tempPosition = currentPosition;
+
         if (getCurrentPosition().getX() == getLastPosition().getX()) {
             currentPosition = new Position(0, getCurrentPosition().getY() + 1);
         }
         else{
             currentPosition = new Position(getCurrentPosition().getX() + 1, getCurrentPosition().getY());
         }
-        return getTInMatrix(getCurrentPosition());
+        return getTInMatrix(tempPosition);
     }
 
     public T getTInMatrix(Position position) {
