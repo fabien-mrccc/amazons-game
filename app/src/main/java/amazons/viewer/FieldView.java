f32c4cbd96773ba54fe23fd9a4d543f758c1c95a
Fanani <amina.fanani@etu.univ-amu.fr>
2023-11-28 17:01:24 +0100

    fix BoardView and FieldView : uncommented

--- app/src/main/java/amazons/viewer/FieldView.java
+++ app/src/main/java/amazons/viewer/FieldView.java
@@ -18,9 +18,8 @@ import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 
-// TODO Uncomment
-// import static amazons.figures.ArrowFigure.ARROW_FIGURE;
-// import static amazons.figures.EmptyFigure.EMPTY_FIGURE;
+ import static amazons.figures.ArrowFigure.ARROW_FIGURE;
+ import static amazons.figures.EmptyFigure.EMPTY_FIGURE;
 
 public class FieldView extends Label {
     private static final String defaultStyleBlack = "-fx-background-color: gray;";
@@ -122,9 +121,8 @@ public class FieldView extends Label {
 
     private String getFigureImageName(Figure figure){
         StringBuilder builder = new StringBuilder("images/");
-        // TODO uncomment
-        // if (figure == EMPTY_FIGURE) throw new IllegalArgumentException("Empty figure has no image");
-        // if (figure == ARROW_FIGURE) return builder.append("cross.png").toString();
+        if (figure == EMPTY_FIGURE) throw new IllegalArgumentException("Empty figure has no image");
+        if (figure == ARROW_FIGURE) return builder.append("cross.png").toString();
         builder.append(figure.getPlayerID().color);
         builder.append("_queen.png");
         return builder.toString();
