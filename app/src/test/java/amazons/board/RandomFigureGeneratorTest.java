c03bda57dd9005d85b8cff3f5ed28f9f2de81c5a
marcuccini <fabien.marcuccini@etu.univ-amu.fr>
2023-11-28 16:49:04 +0100

    add int numberOfFiguresToAssign()

---
+++ app/src/test/java/amazons/board/RandomFigureGeneratorTest.java
@@ -0,0 +1,13 @@
+package amazons.board;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+
+public class RandomFigureGeneratorTest {
+
+    @Test
+    int numberOfFiguresToAssign(){
+        return 0;
+    }
+}
