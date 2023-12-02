893b94a383edee12e76e4d40d2fcdcb1b3a099f9
marcuccini <fabien.marcuccini@etu.univ-amu.fr>
2023-12-02 17:49:59 +0100

    add void nextFigure()

--- app/src/test/java/amazons/board/RandomFigureGeneratorTest.java
+++ app/src/test/java/amazons/board/RandomFigureGeneratorTest.java
@@ -1,8 +1,12 @@
 package amazons.board;
 
 import amazons.figures.Amazon;
+import static amazons.figures.EmptyFigure.EMPTY_FIGURE;
+
+import amazons.figures.EmptyFigure;
 import amazons.figures.Figure;
 import amazons.figures.MovableFigure;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,26 +19,77 @@ public class RandomFigureGeneratorTest {
 
     private final Random random = new Random();
     private final List<MovableFigure> movableFigures = new ArrayList<>();
-    private final MatrixBoard matrixBoard = new MatrixBoard(10,10);
-    private Iterator<Position> iterator = matrixBoard.positionIterator();
-    private final Set<Figure> usedFigures = new HashSet<>();
+    private final MatrixBoard matrixBoard = new MatrixBoard(2,3);
+    private final Iterator<Position> iterator = matrixBoard.positionIterator();
 
-    private RandomFigureGenerator randomFigureGenerator;
+    //private final Set<Figure> usedFigures = new HashSet<>();
 
     @BeforeEach
     void setUp(){
         movableFigures.add(new Amazon(new Position(0,0),0));
         movableFigures.add(new Amazon(new Position(1,0),0));
-        movableFigures.add(new Amazon(new Position(2,0),0));
-        movableFigures.add(new Amazon(new Position(3,0),1));
-        movableFigures.add(new Amazon(new Position(4,0),1));
-        movableFigures.add(new Amazon(new Position(5,0),1));
-
-        randomFigureGenerator = new RandomFigureGenerator(random, movableFigures, iterator);
+        movableFigures.add(new Amazon(new Position(0,1),0));
+        movableFigures.add(new Amazon(new Position(1,1),1));
+        movableFigures.add(new Amazon(new Position(0,2),1));
+        movableFigures.add(new Amazon(new Position(1,2),1));
     }
 
     @Test
     void nextFigure(){
+        RandomFigureGenerator randomFigureGenerator = new RandomFigureGenerator(random, movableFigures, iterator);
+
+        // no loop below this comment because of index which as to be final in movablesFigures.get(index) (for the Assertions class)
+
+        assertThat(randomFigureGenerator.nextFigure(new Position(3,9))).satisfiesAnyOf(
+                value -> Assertions.assertThat(value).isEqualTo(EMPTY_FIGURE),
+                value -> Assertions.assertThat(value).isEqualTo(movableFigures.get(5)));
+
+        assertThat(randomFigureGenerator.nextFigure(new Position(3,9))).satisfiesAnyOf(
+                value -> Assertions.assertThat(value).isEqualTo(EMPTY_FIGURE),
+                value -> Assertions.assertThat(value).isEqualTo(movableFigures.get(4)));
+
+        assertThat(randomFigureGenerator.nextFigure(new Position(3,9))).satisfiesAnyOf(
+                value -> Assertions.assertThat(value).isEqualTo(EMPTY_FIGURE),
+                value -> Assertions.assertThat(value).isEqualTo(movableFigures.get(3)));
+
+        assertThat(randomFigureGenerator.nextFigure(new Position(3,9))).satisfiesAnyOf(
+                value -> Assertions.assertThat(value).isEqualTo(EMPTY_FIGURE),
+                value -> Assertions.assertThat(value).isEqualTo(movableFigures.get(2)));
+
+        assertThat(randomFigureGenerator.nextFigure(new Position(3,9))).satisfiesAnyOf(
+                value -> Assertions.assertThat(value).isEqualTo(EMPTY_FIGURE),
+                value -> Assertions.assertThat(value).isEqualTo(movableFigures.get(1)));
+
+        assertThat(randomFigureGenerator.nextFigure(new Position(3,9))).satisfiesAnyOf(
+                value -> Assertions.assertThat(value).isEqualTo(movableFigures.get(0)));
+
+        final MatrixBoard matrixBoard100x100 = new MatrixBoard(100,100);
+        final Iterator<Position> iterator100x100 = matrixBoard100x100.positionIterator();
+
+        movableFigures.add(new Amazon(new Position(0,3),0));
+        movableFigures.add(new Amazon(new Position(1,3),1));
+
+        randomFigureGenerator = new RandomFigureGenerator(random, movableFigures, iterator100x100);
+        Figure figure;
+        int emptyFigureCount = 0;
+        int amazonFigureCount = 0;
+        int strangeFigureCount = 0;
+
+        for(int i=0; i <10000; i++){
+            figure = randomFigureGenerator.nextFigure(new Position(0,0));
+            if(figure instanceof EmptyFigure){
+                emptyFigureCount++;
+            }
+            else if (figure instanceof Amazon){
+                amazonFigureCount++;
+            }
+            else{
+                strangeFigureCount++;
+            }
+        }
+        assertThat(emptyFigureCount).isEqualTo(9992);
+        assertThat(amazonFigureCount).isEqualTo(8);
+        assertThat(strangeFigureCount).isEqualTo(0);
     }
 
     // test private methods
