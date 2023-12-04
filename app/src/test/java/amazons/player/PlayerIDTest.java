65308a06573c7a7643df8119c58626499ca3bd27
FANANI Amina <amina.fanani@etu.univ-amu.fr>
2023-12-04 11:11:53 +0100

    fix void testToString() add void testOpponent()

--- app/src/test/java/amazons/player/PlayerIDTest.java
+++ app/src/test/java/amazons/player/PlayerIDTest.java
@@ -1,12 +1,18 @@
 package amazons.player;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
+import static amazons.player.PlayerID.*;
 public class PlayerIDTest {
     @Test
     void testToString(){
-        assertThat(PlayerID.PLAYER_ONE.toString()).isEqualTo("player 1 (black)");
-        assertThat(PlayerID.PLAYER_ZERO.toString()).isEqualTo("player 0 (white)");
-        assertThat(PlayerID.NONE.toString()).isEqualTo("player -1 ()");
+        assertThat(PLAYER_ONE.toString()).isEqualTo("player 1 (black)");
+        assertThat(PLAYER_ZERO.toString()).isEqualTo("player 0 (white)");
+        assertThat(NONE.toString()).isEqualTo("player -1 ()");
+    }
+    @Test
+    void testOpponent(){
+        assertThat(PLAYER_ZERO.opponent()).isEqualTo(PLAYER_ONE);
+        assertThat(PLAYER_ONE.opponent()).isEqualTo(PLAYER_ZERO);
+        assertThat(NONE.opponent()).isEqualTo(NONE);
     }
 }
