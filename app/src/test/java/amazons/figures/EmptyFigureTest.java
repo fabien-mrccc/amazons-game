b67a21ab3d1984c515181f789d12b9fb0c34fd29
marcuccini <fabien.marcuccini@etu.univ-amu.fr>
2023-12-04 12:19:52 +0100

    fix void testCanMoveTo(): add limit case and normal case testing

--- app/src/test/java/amazons/figures/EmptyFigureTest.java
+++ app/src/test/java/amazons/figures/EmptyFigureTest.java
@@ -1,18 +1,29 @@
 package amazons.figures;
 
+import amazons.IllegalMoveException;
+import amazons.board.MatrixBoard;
 import amazons.board.Position;
 import amazons.player.PlayerID;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
+import static amazons.figures.EmptyFigure.*;
 
 public class EmptyFigureTest {
     @Test
-    void testGetPlayerID(){
-        assertThat(EmptyFigure.EMPTY_FIGURE.getPlayerID()).isEqualTo(PlayerID.NONE);
+    void testCanMoveTo(){
+        assertThat(EMPTY_FIGURE.canMoveTo(null, null)).isEqualTo(false);
+        assertThat(EMPTY_FIGURE.canMoveTo(new Position(0,0), new MatrixBoard(3,3))).isEqualTo(false);
     }
 
     @Test
-    void testCanMoveTo(){
-        assertThat(EmptyFigure.EMPTY_FIGURE.canMoveTo(new Position (0,0), null)).isEqualTo(false);
+    void testMoveTo(){
+        assertThatExceptionOfType(IllegalMoveException.class)
+                .isThrownBy(() -> EMPTY_FIGURE.moveTo(null,null))
+                .withMessageMatching("A neutral figure can't be moved");
     }
+    @Test
+    void testGetPlayerID(){
+        assertThat(EMPTY_FIGURE.getPlayerID()).isEqualTo(PlayerID.NONE);
+    }
+
 }
