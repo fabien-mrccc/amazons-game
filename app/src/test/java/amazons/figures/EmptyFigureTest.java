88ae8aabeb9781110c0a707eefa5aa55a9ecb248
marcuccini <fabien.marcuccini@etu.univ-amu.fr>
2023-12-05 14:49:28 +0100

    add void testMoveTo()

--- app/src/test/java/amazons/figures/EmptyFigureTest.java
+++ app/src/test/java/amazons/figures/EmptyFigureTest.java
@@ -1,6 +1,6 @@
 package amazons.figures;
 
-import amazons.IllegalMoveException;
+import amazons.board.Board;
 import amazons.board.MatrixBoard;
 import amazons.board.Position;
 import amazons.player.PlayerID;
@@ -9,18 +9,26 @@ import static org.assertj.core.api.Assertions.*;
 import static amazons.figures.EmptyFigure.*;
 
 public class EmptyFigureTest {
+
+    private final Board board = new MatrixBoard(3,3);
+    private final Position originPosition = new Position(0,0);
+
     @Test
     void testCanMoveTo(){
         assertThat(EMPTY_FIGURE.canMoveTo(null, null)).isEqualTo(false);
-        assertThat(EMPTY_FIGURE.canMoveTo(new Position(0,0), new MatrixBoard(3,3))).isEqualTo(false);
+        assertThat(EMPTY_FIGURE.canMoveTo(originPosition, board)).isEqualTo(false);
     }
 
     @Test
     void testMoveTo(){
-        assertThatExceptionOfType(IllegalMoveException.class)
-                .isThrownBy(() -> EMPTY_FIGURE.moveTo(null,null))
-                .withMessageMatching("A neutral figure can't be moved");
+        assertThatThrownBy(() -> EMPTY_FIGURE.moveTo(null, null))
+                .isInstanceOf(IllegalMoveException.class)
+                .hasMessage("A neutral figure can't be moved");
+        assertThatThrownBy(() -> EMPTY_FIGURE.moveTo(originPosition, board))
+                .isInstanceOf(IllegalMoveException.class)
+                .hasMessage("A neutral figure can't be moved");
     }
+
     @Test
     void testGetPlayerID(){
         assertThat(EMPTY_FIGURE.getPlayerID()).isEqualTo(PlayerID.NONE);
