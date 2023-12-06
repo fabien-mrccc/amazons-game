d0ecdfe3503c1bb03cea8ad1fc2a4cd13d14d9f9
marcuccini <fabien.marcuccini@etu.univ-amu.fr>
2023-12-06 10:09:37 +0100

    add protected Amazon getAmazonToMove() add private Amazon getRandomAmazonOfAIPlayer(List<Amazon> movableAmazons) add private List<Amazon> getMovableAmazons()

--- app/src/main/java/amazons/player/AIBasicPlayer.java
+++ app/src/main/java/amazons/player/AIBasicPlayer.java
@@ -1,9 +1,37 @@
 package amazons.player;
 
-public class AIBasicPlayer extends AbstractPlayer{
+import amazons.figures.Amazon;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class AIBasicPlayer extends AbstractAIPlayer{
 
     @Override
     public Move play(Move opponentMove) {
+        Amazon amazonToMove = getAmazonToMove();
         return null;
+    }
+
+    @Override
+    protected Amazon getAmazonToMove() {
+        return null;
+    }
+
+    private Amazon getRandomAmazonOfAIPlayer(List<Amazon> movableAmazons){
+        Random random = new Random();
+        int randomAmazonIndex = random.nextInt() % aiPLayerAmazons.size();
+        return aiPLayerAmazons.get(randomAmazonIndex);
+    }
+
+    private List<Amazon> getMovableAmazons(){
+        List<Amazon> movableAmazons = new ArrayList<>();
+        for(Amazon amazon : aiPLayerAmazons){
+            if(amazon.getAccessiblePositions(aiBoardRepresentation).size() > 0){
+                movableAmazons.add(amazon);
+            }
+        }
+        return movableAmazons;
     }
 }
