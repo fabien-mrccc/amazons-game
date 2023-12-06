c00d78278937fd2879fa60a48d0472f65a6aaf83
marcuccini <fabien.marcuccini@etu.univ-amu.fr>
2023-12-06 09:26:49 +0100

    add private void instantiateAiBoard(List<Position>[] initialPositions)

--- app/src/main/java/amazons/player/AbstractAIPlayer.java
+++ app/src/main/java/amazons/player/AbstractAIPlayer.java
@@ -15,10 +15,15 @@ public abstract class AbstractAIPlayer extends AbstractPlayer{
     @Override
     public void initialize(int boardWidth, int boardHeight,  PlayerID playerID, List<Position>[] initialPositions) {
         super.initialize(boardWidth, boardHeight, playerID, initialPositions);
+        instantiateAIBoard(initialPositions);
+    }
+
+    private void instantiateAIBoard(List<Position>[] initialPositions){
         aiBoardRepresentation = new MatrixBoard(boardWidth,boardHeight);
         PresetFigureGenerator generator = new PresetFigureGenerator(Game.createPlayersFiguresWithDefaultPosition(initialPositions));
         aiBoardRepresentation.fill(generator);
     }
+
     @Override
     public abstract Move play(Move opponentMove);
 }
