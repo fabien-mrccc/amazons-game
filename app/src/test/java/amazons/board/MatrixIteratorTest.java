4e4350c2a3acf886350ee379cfa835197607d511
marcuccini <fabien.marcuccini@etu.univ-amu.fr>
2023-12-06 06:24:48 +0100

    add void hasNext() fix void setUp()

--- app/src/test/java/amazons/board/MatrixIteratorTest.java
+++ app/src/test/java/amazons/board/MatrixIteratorTest.java
@@ -4,30 +4,73 @@ import amazons.figures.Figure;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static amazons.figures.EmptyFigure.*;
+import static org.assertj.core.api.Assertions.*;
 
 public class MatrixIteratorTest {
-    private final int numberOfColumns = 3;
-    private final int numberOfRows = 3;
-    private Position[][] positions;
-    private Figure[][] figures;
-    private MatrixIterator positionsIterator;
-    private MatrixIterator figuresIterator;
-
+    private MatrixIterator<Position> positionsIterator;
+    private MatrixIterator<Figure> figuresIterator;
+    private MatrixIterator<Position> nullIterator;
 
     @BeforeEach
     void setUp(){
-        for(int x = 0; x < 5; x++) {
-            for (int y = 0; y < 5; y++) {
+        final int numberOfColumns = 3;
+        final int numberOfRows = 3;
+        final Position[][] positions = new Position[numberOfColumns][numberOfRows];
+        final Figure[][] figures = new Figure[numberOfColumns][numberOfRows];
+
+        for(int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
                 positions[x][y] = new Position(x,y);
                 figures[x][y] = EMPTY_FIGURE;
             }
         }
         positionsIterator = new MatrixIterator<>(numberOfColumns, numberOfRows, positions);
         figuresIterator = new MatrixIterator<>(numberOfColumns, numberOfRows, figures);
+        nullIterator = new MatrixIterator<>(0,0,null);
     }
 
     @Test
     void hasNext(){
+        assertThat(positionsIterator.hasNext()).isTrue();
+        positionsIterator.next();
+        assertThat(positionsIterator.hasNext()).isTrue();
+        positionsIterator.next();
+        assertThat(positionsIterator.hasNext()).isTrue();
+        positionsIterator.next();
+        assertThat(positionsIterator.hasNext()).isTrue();
+        positionsIterator.next();
+        assertThat(positionsIterator.hasNext()).isTrue();
+        positionsIterator.next();
+        assertThat(positionsIterator.hasNext()).isTrue();
+        positionsIterator.next();
+        assertThat(positionsIterator.hasNext()).isTrue();
+        positionsIterator.next();
+        assertThat(positionsIterator.hasNext()).isTrue();
+        positionsIterator.next();
+        assertThat(positionsIterator.hasNext()).isTrue();
+        positionsIterator.next();
+        assertThat(positionsIterator.hasNext()).isFalse();
 
+        assertThat(figuresIterator.hasNext()).isTrue();
+        figuresIterator.next();
+        assertThat(figuresIterator.hasNext()).isTrue();
+        figuresIterator.next();
+        assertThat(figuresIterator.hasNext()).isTrue();
+        figuresIterator.next();
+        assertThat(figuresIterator.hasNext()).isTrue();
+        figuresIterator.next();
+        assertThat(figuresIterator.hasNext()).isTrue();
+        figuresIterator.next();
+        assertThat(figuresIterator.hasNext()).isTrue();
+        figuresIterator.next();
+        assertThat(figuresIterator.hasNext()).isTrue();
+        figuresIterator.next();
+        assertThat(figuresIterator.hasNext()).isTrue();
+        figuresIterator.next();
+        assertThat(figuresIterator.hasNext()).isTrue();
+        figuresIterator.next();
+        assertThat(figuresIterator.hasNext()).isFalse();
+
+        assertThat(nullIterator.hasNext()).isFalse();
     }
 }
