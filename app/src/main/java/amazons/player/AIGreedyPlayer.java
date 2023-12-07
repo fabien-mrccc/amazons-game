0f4c03372607a23b15ecadd59b05f86882048295
m22027323 <fabien.marcuccini@etu.univ-amu.fr>
2023-12-07 11:02:33 +0100

    fix protected Position destPositionOfAmazonToMove(Position startPosition) fix protected Position destPositionOfArrowToShoot(Position startPosition) fix protected Position startPositionOfAmazonToMove()

--- app/src/main/java/amazons/player/AIGreedyPlayer.java
+++ app/src/main/java/amazons/player/AIGreedyPlayer.java
@@ -2,10 +2,11 @@ package amazons.player;
 
 import amazons.board.Position;
 import amazons.figures.Amazon;
-import amazons.util.RandomUtil.*;
+import static amazons.util.RandomUtil.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class AIGreedyPlayer extends AbstractAIPlayer {
 
@@ -16,20 +17,13 @@ public class AIGreedyPlayer extends AbstractAIPlayer {
 
     @Override
     protected Position destPositionOfAmazonToMove(Position startPosition) {
-        return null;
+        return getRandomElement(new Random(), bestAmazonDestinations(bestAmazonToMove()));
     }
 
     @Override
     protected Position destPositionOfArrowToShoot(Position startPosition) {
-        return null;
+        return getRandomElement(new Random(), bestShootPositions(new Amazon(destPositionOfArrowToShoot(bestAmazonToMove().getPosition()),playerID.index)));
     }
-
-    /* maximiser la possibilité de gagner: si l'une de mes amazones est ent
-    entourer par une ou plusieurs flèche ou amazones adverses la faire bouger à
-    une case vide et entourer de cases vide, sinon aller dans la case la moins entourer.
-    minimiser les choix de l'adversaire: chercher l'amazone du joueur adverse avec
-    le plus de cases accessible et envoyer la flèche à l'une de ses cases adjacentes..
-     */
 
     /**
      * return the number of available positions in the board of the game for an amazon given in parameters
