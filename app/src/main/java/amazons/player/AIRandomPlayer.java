9262be0b7c5fa21eac8c5a1496ed9b54b25ccfca
FANANI Amina <amina.fanani@etu.univ-amu.fr>
2023-12-07 21:41:37 +0100

    fix protected Position startPositionOfAmazonToMove()

--- app/src/main/java/amazons/player/AIRandomPlayer.java
+++ app/src/main/java/amazons/player/AIRandomPlayer.java
@@ -1,4 +1,23 @@
 package amazons.player;
 
-public class AIRandomPlayer {
+import amazons.board.Position;
+import amazons.figures.Amazon;
+import static amazons.util.RandomUtil.*;
+import java.util.Random;
+public class AIRandomPlayer extends AbstractAIPlayer {
+    @Override
+    protected Position startPositionOfAmazonToMove() {
+        Amazon amazonToMove = getRandomElement(new Random(),getMovableAmazons());
+        return amazonToMove.getPosition();
+    }
+
+    @Override
+    protected Position destPositionOfAmazonToMove(Position startPosition) {
+        return null;
+    }
+
+    @Override
+    protected Position destPositionOfArrowToShoot(Position startPosition) {
+        return null;
+    }
 }
