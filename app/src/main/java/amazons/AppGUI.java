15c02b02bf4c03fa41c8c967be0da3b8e5683d05
marcuccini <fabien.marcuccini@etu.univ-amu.fr>
2023-12-07 23:53:51 +0100

    update value pause to 1500 milliseconds

--- app/src/main/java/amazons/AppGUI.java
+++ app/src/main/java/amazons/AppGUI.java
@@ -25,7 +25,7 @@ public class AppGUI extends Application {
     @Override
     public void start(Stage primaryStage) {
         Game game = new Game();
-        GameController controller = new GameController(game, new GUIPLayer(), new GUIPLayer());
+        GameController controller = new GameController(game, new AIGreedyPlayer(), new AIGreedyPlayer());
         int numberOfColumns =  game.getNumberOfColumns();
         int numberOfRows =  game.getNumberOfRows();
 
